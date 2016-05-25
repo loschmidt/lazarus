195bf89b550e625ef0ed824db77c799f71a63ab6
Victor Hanson-Smith <victor@MacBook-Air.local>
2016-05-24 19:23:44 -0700

    .

---
+++ DOCUMENTATION/example_data/run_mcm1_example.sh
@@ -0,0 +1,2 @@
+python ~/Documents/SourceCode/Lazarus/lazarus.py --baseml --alignment mcm1codalign.fasta --getanc --outgroup [Zrou_Mc05] --ingroup [Vpol_Mc08,Ylip_MA21] --model 'HKY85' --branch_length 'estimate' --outputdir ./ --tree mcm1codalign.mltree.tre --alpha 0.5
+
